05f185540e783e802745f197099ea57f2ab5b58f
vNakama <nakama@alunos.utfpr.edu.br>
2019-09-17 21:49:51 -0300

    definindo metodos finais

--- src/main/java/br/edu/utfpr/dv/siacoes/dao/Template.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/dao/Template.java
@@ -4,15 +4,15 @@ import br.edu.utfpr.dv.siacoes.model.SigacConfig;
 import br.edu.utfpr.dv.siacoes.model.SigesConfig;
 
 public abstract class TemplateDAO {
-    public T findByDepartment(int idDepartment){
+    public final T findByDepartment(int idDepartment){
         return null;
     }
 
-    public int save(int idUser, T config) {
+    public final int save(int idUser, T config) {
         return 0;
     }
 
-    private T loadObject(ResultSet rs){
+    public final T loadObject(ResultSet rs){
         return null;
     }
 }
