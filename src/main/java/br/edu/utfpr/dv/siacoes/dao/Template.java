dbeaa0ac54631fe258a863680be9f0ab9ee2b74c
vNakama <nakama@alunos.utfpr.edu.br>
2019-09-17 22:00:35 -0300

    transferindo metodos comuns para superclasse

--- src/main/java/br/edu/utfpr/dv/siacoes/dao/Template.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/dao/Template.java
@@ -1,18 +1,86 @@
 package br.edu.utfpr.dv.siacoes.dao;
 
+import br.edu.utfpr.dv.siacoes.log.UpdateEvent;
 import br.edu.utfpr.dv.siacoes.model.SigacConfig;
 import br.edu.utfpr.dv.siacoes.model.SigesConfig;
 
+import java.lang.reflect.GenericArrayType;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class TemplateDAO {
     public final T findByDepartment(int idDepartment){
-        return null;
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try{
+            conn = ConnectionDAO.getInstance().getConnection();
+            stmt = conn.prepareStatement(sqlStm);
+
+            stmt.setInt(1, idDepartment);
+
+            rs = stmt.executeQuery();
+
+            if(rs.next()){
+                return this.loadObject(rs);
+            }else{
+                return null;
+            }
+        }finally{
+            if((rs != null) && !rs.isClosed())
+                rs.close();
+            if((stmt != null) && !stmt.isClosed())
+                stmt.close();
+            if((conn != null) && !conn.isClosed())
+                conn.close();
+        }
     }
 
     public final int save(int idUser, T config) {
-        return 0;
+        boolean insert = (this.findByDepartment(config.getDepartment().getIdDepartment()) == null);
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try{
+            conn = ConnectionDAO.getInstance().getConnection();
+
+            if(insert){
+                stmt = conn.prepareStatement(sqlInsertStm);
+            }else{
+                stmt = conn.prepareStatement(sqlUpdateStm);
+            }
+
+            stmt.setDouble(1, config.getMinimumScore());
+            stmt.setInt(2, config.getMaxFileSize());
+            stmt.setInt(3, config.getDepartment().getIdDepartment());
+
+            stmt.execute();
+
+            new UpdateEvent(conn).registerUpdate(idUser, config);
+
+            return config.getDepartment().getIdDepartment();
+        }finally{
+            if((stmt != null) && !stmt.isClosed())
+                stmt.close();
+            if((conn != null) && !conn.isClosed())
+                conn.close();
+        }
     }
 
     public final T loadObject(ResultSet rs){
-        return null;
+        T config = new SigacConfig();
+
+        config.getDepartment().setIdDepartment(rs.getInt("idDepartment"));
+        config.setMinimumScore(rs.getDouble("minimumScore"));
+        config.setMaxFileSize(rs.getInt("maxfilesize"));
+
+        return config;
     }
 }
